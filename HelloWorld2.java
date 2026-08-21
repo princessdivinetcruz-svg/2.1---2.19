5c52c347ac0ce91b93555d0ebc80b90f87202da8
princessdivinetcruz-svg <princessdivine.t.cruz@isu.edu.ph>
2026-08-21 18:49:34 +0800

    Add files via upload

---
+++ HelloWorld2.java
@@ -0,0 +1,15 @@
+// Filename HelloWorld2.java
+// Written by Princess Divine Cruz
+// BSIT NETSEC 1-2
+// Written on August 21, 2026
+public class HelloWorld2{
+    /*
+This class demonstrates the use of the print() and println()
+method to print the message Hello, world! and I am a new student.
+I'm ready for Java programming.
+*/
+    public static void main(String[] args) {
+        System.out.print("Hello, world! ");
+        System.out.println("I am a new student.");
+    }
+}
