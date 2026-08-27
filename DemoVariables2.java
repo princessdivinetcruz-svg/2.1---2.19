2dd5a279d981cfc92700aac45cbd7b47ef35854c
princessdivinetcruz-svg <princessdivine.t.cruz@isu.edu.ph>
2026-08-27 16:35:34 +0800

    Add files via upload

---
+++ DemoVariables2.java
@@ -0,0 +1,17 @@
+
+package helloworld.variables;
+
+public class DemoVariables2 {
+     public static void main(String[] args)
+    {
+          int intNo = 296;
+          short shortNo = 19;
+          long longNo = 1234567876543L;
+           System.out.print("The int is ");
+           System.out.println(intNo);
+           System.out.print("The short is ");
+           System.out.println(shortNo);
+           System.out.print("The long is ");
+           System.out.println(longNo);
+    }
+}
