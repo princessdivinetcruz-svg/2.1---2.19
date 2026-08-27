c117e8900bf493c1b0591acda458173a97e98218
princessdivinetcruz-svg <princessdivine.t.cruz@isu.edu.ph>
2026-08-27 16:31:29 +0800

    Add files via upload

---
+++ DemoVariables.java
@@ -0,0 +1,11 @@
+
+package helloworld.variables;
+
+public class DemoVariables {
+     public static void main(String[] args)
+    {
+          int intNo = 296;
+           System.out.print("The int is ");
+           System.out.println(intNo);
+    }
+}
